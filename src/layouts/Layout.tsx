4883982b8feaee0d7dd0161e0b14e6a9542cd6af
jolinhappy <jolinhappy1990@hotmail.com>
2022-05-01 16:01:23 +0800

    add layout

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LayoutComponent = ({ className, navbar, main }: any) => {
+  return (
+    <div className={ className }>
+      <div className="layout__container">
+        <div className="layout-item__nav">
+          { navbar }
+        </div>
+        <div className="layout-item__main">
+          { main }
+        </div>
+      </div>
+    </div>
+  )
+};
+
+const Layout = styled(LayoutComponent)`
+  height: 100%;
+  .layout__container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    .layout-item__nav {
+      height: 50px;
+    }
+    .layout-item__main {
+      flex: 1;
+    }
+  }
+`;
+
+export default Layout;
